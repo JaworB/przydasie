fb3c43bbaa4a5954d958ab46aeb4b7e5e68883ac
root <root@sandbox>
2023-08-15 12:57:31 +0200

    Added VMs update playbook

--- scripts/bash/backup.sh
+++ scripts/bash/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 conf_file=./config
 backup_dir=/backup
-dest_dir=$backup_dir/backup_$(date +%Y%m%d)/
+dest_dir=$backup_dir/backup_$(date +%Y%m%d)
 fs_to_backup="docker_services vmstorage"
 dir_to_construct="$dest_dir $dest_dir/$fs_to_backup"
 
@@ -19,26 +19,34 @@ virsh_dump () {
     vm_list=$(virsh list --all |awk '{print $2}'|sed 's/Name//')
     for vm in $vm_list; do 
 			vmstate=$(virsh list --all |grep $vm |awk '{print $3}')
-			if [ $vmstate == "running" ];then
-				virsh shutdown $vm > /dev/null
-			fi
+#			if [ $vmstate == "running" ];then
+#				#virsh shutdown $vm > /dev/null
+#				#temoporary disabld shutdown check for hosts
+#			fi
       rm -f /vmstorage/xml/*
       virsh dumpxml $vm >> /vmstorage/xml/$vm.xml
 		done
     echo "=== VMs xmls backed up ==="
 }
 
-rsync () {
+copy () {
   echo "=== Backup started ==="
   for fs in $fs_to_backup;do
     echo "=== Backup of $fs in progress ==="
-    cp -pr /$fs/* $dest_dir$fs
+    cp -pr /$fs/* $dest_dir/$fs
     echo "Backup of $fs finished"
     done
   echo "Backup process successfully finished"
   lab vms start
 }
 
+archive () {
+  echo "Compressing $dest_dir to $dest_dir.tar.gz"
+  tar -zcf $dest_dir.tar.gz $dest_dir
+  rm -rf $dest_dir
+}
+
 create_dirs
 virsh_dump
-rsync
+copy
+archive
